4997063caa3fff4c60dfe4eb792196dc8f4a35ca
Paulo Rabelo <paulorabelo@hotmail.com>
2022-02-14 21:38:54 -0300

    Configurado CheckBox na interface.

--- app/src/main/java/br/com/mrgsoft/toggleeswitchprojeto/MainActivity.java
+++ app/src/main/java/br/com/mrgsoft/toggleeswitchprojeto/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private ToggleButton toggleSenha;
     private Switch switchSenha;
     private TextView textResultado;
+    private CheckBox checkSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +24,20 @@ public class MainActivity extends AppCompatActivity {
         toggleSenha     = findViewById(R.id.toggleSenha);
         switchSenha     = findViewById(R.id.switchSenha);
         textResultado   = findViewById(R.id.textResultado);
+        checkSenha      = findViewById(R.id.checkSenha);
+
     }
+
+
 
     public void enviar(View view){
 
-        if ( switchSenha.isChecked() ){
-            textResultado.setText("Switch ligado");
+        //if ( toggleSenha.isChecked() ){
+        //if ( switchSenha.isChecked() ){
+        if ( checkSenha.isChecked() ) {
+            textResultado.setText("check ligado");
         }else {
-            textResultado.setText("Switch desligado");
+            textResultado.setText("check desligado");
         }
     }
 }
